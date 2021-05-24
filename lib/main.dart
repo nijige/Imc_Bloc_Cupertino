2a348f3d273d71daa38aae449ef430160f5de801
Geane Ferreira Pereira <geane3096@gmail.com>
2021-05-24 19:40:55 -0400

    Primeiro Commit

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:imc/ui/android/material-app.dart';
+
+void main() => runApp(MyMaterialApp());
---
+++ lib/ui/ios/cupertino-app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:imc/ui/android/pages/home_page.dart';
+
+class MyCupertinoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      title: "IMC",
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      
+    );
+  }
+}
